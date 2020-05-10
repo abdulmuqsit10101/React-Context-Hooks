d72aba50f4dc28602c362dbd9bc87046a13d7d78
Ali <abdulmuqsit10101@gmail.com>
2020-05-11 02:07:20 +0500

    [components/BookDetails]: list-item ( li ) on click event BookContext's removeFromBooks funtion is working now

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {BookContext} from '../context/BookContext.js';
 
 function BookDetails({book}) {
-  const { title, author } = book;
+  const {title, author} = book;
+  const {removeFromBooks} = useContext(BookContext);
   return (
-    <li className="list-item">
+    <li className="list-item" onClick={() => removeFromBooks(book.id)}>
       <div>{title}</div>
       <div>{author}</div>
     </li>
