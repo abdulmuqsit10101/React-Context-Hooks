23c279682ab771e8a3de9d98a59baacefd544f65
Ali <abdulmuqsit10101@gmail.com>
2020-05-11 00:53:25 +0500

    [src/App.js]: BookList component is added in BookContextProvider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import './App.css';
 import Navbar from './components/Navbar.js';
 import BookContextProvider from './context/BookContext.js';
+import './App.css';
+import BookList from './components/BookList.js';
 
 function App() {
   return (
     <div className="App">
       <BookContextProvider>
         <Navbar/>
+        <BookList/>
       </BookContextProvider>
     </div>
   );
 }
 
 export default App;
+
