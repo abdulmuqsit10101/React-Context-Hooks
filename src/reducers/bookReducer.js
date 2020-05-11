3cbf4ae8a3b0884f2083af6c3c22bef2bd194570
Ali <abdulmuqsit10101@gmail.com>
2020-05-11 14:05:35 +0500

    [reducers/bookReducer.js]: A reducer created to manage bookContext states and methods

---
+++ src/reducers/bookReducer.js
@@ -0,0 +1,17 @@
+import {ADD_BOOK, REMOVE_BOOK} from './actionTypes.js';
+import {v1 as uuidv1} from "uuid";
+
+export const bookReducer = (state, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, {
+        title: action.book.title,
+        author: action.book.author,
+        id: uuidv1()
+      }];
+    case REMOVE_BOOK:
+      return state.filter(book => (book.id !== action.id));
+    default:
+      return state;
+  }
+};
